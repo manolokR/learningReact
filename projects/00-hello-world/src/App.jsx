9ae4d30f5632093be1a4dfde223c4759badef2a8
Manuel Ruiz Toribio <mrt00033@red.ujaen.es>
2024-08-08 11:57:10 +0200

    First Comit

---
+++ projects/00-hello-world/src/App.jsx
@@ -0,0 +1,30 @@
+export function App(){
+
+    return(
+      <article>
+        <header>
+           <img alt="El avatar de ansufatidico" src="https://unavatar.io/ansufatidico" />
+           <div>
+                <strong>Ansufatidico Rey CiberTroll</strong>
+                <span>@ansufatidico</span>
+
+           </div>
+
+
+        </header>
+
+        <aside>
+            <button>
+                Seguir   
+                
+            </button>    
+            
+        
+        </aside>    
+
+      </article>
+
+    )
+
+
+}
