c4f945bf9deff0d0b0177f06ab4bf1ea8d87d4e4
Manuel Ruiz Toribio <mrt00033@red.ujaen.es>
2024-08-10 17:06:06 +0200

    Follow pointer done with useEffect logic

--- projects/02-mouse-follower/src/App.jsx
+++ projects/02-mouse-follower/src/App.jsx
@@ -1,8 +1,69 @@
+import { useEffect, useState } from "react"
+
+const FollowMouse = () => {
+  //We must be able to enable and disable the effect
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('effect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY }) // Update the position
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    //Clean up
+    //We need to remove the event listener or subscriptions when the component is unmounted
+    //or when the depebdencies change
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}>
+
+
+      </div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Disable' : 'Enable'} Follow pointer
+      </button>
+
+    </>
+
+
+  )
+
+}
+
 
 function App() {
 
+  
+
+
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <FollowMouse />
+    </main>
+
   )
 }
 
