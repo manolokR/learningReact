40b52a30e790a46e676c72d921f8e32b99c0cbcf
Manuel Ruiz Toribio <mrt00033@red.ujaen.es>
2024-08-10 12:08:58 +0200

    Visual game logic and turns selector

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// Importing necessary modules from React. 'useState' is a hook used to manage state in functional components.
+// 'Children' is a utility from React that provides methods to deal with this.props.children.
+import { Children, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS={
+  X: 'x',
+  O: 'o',
+
+}
+
+
+// This component will be used to render each square of the Tic-Tac-Toe board.
+const Square = ({children, isSelected ,updateBoard, index}) => {
+  // Dynamically set the class name based on whether the square is selected.
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+
+      updateBoard(index)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onClick={handleClick} className={className}>
+        {children}
+    </div>
   )
+
+}
+
+// Main App component, which will hold the entire game logic and UI.
+function App() {
+
+  const [board,setBoard] = useState(Array(9).fill(null))
+
+  console.log(board)
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn //X or O
+    setBoard(newBoard)
+
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
+  return (
+    <main className='board'>
+
+      <h1>Tic tac toe</h1>
+
+      <section className='game'>
+        {
+          board.map((_, index) => {
+
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+
+      </section>
+
+      <section  className='turn'>
+        
+        <Square isSelected={turn == TURNS.X}>  {TURNS.X} </Square>
+        <Square isSelected={turn == TURNS.O}>  {TURNS.O} </Square>
+      </section>    
+
+    </main>
+
+
+  )
+
 }
 
 export default App
