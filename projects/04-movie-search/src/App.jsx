30d53716b5dba132729c23cf27ffe854aaf50e08
Manuel Ruiz Toribio <mrt00033@red.ujaen.es>
2024-08-17 12:36:18 +0200

    Added custom hook and useRef hook for handling search input

--- projects/04-movie-search/src/App.jsx
+++ projects/04-movie-search/src/App.jsx
@@ -2,24 +2,73 @@
 import './App.css'
 import { useMovies } from './hooks/useMovies.js'
 import { Movies } from './components/Movies.jsx'
-import { useRef } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
+
+// Custom hook to handle search input
+function useSearch(){
+  const [search,updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  //useRef to check if it is the first input and not show error message
+  const isFirstInput = useRef(true)
+
+
+  useEffect(() => {
+
+    if(isFirstInput.current){
+      //set isFirstInput to false after first input
+      isFirstInput.current = search==''
+      return
+    }
+
+    if(search==''){
+
+      setError('Please enter a movie name') 
+      return
+
+    }
+
+    if(search.match(/^\d+$/)){
+
+      setError('Please enter a valid movie name') 
+      return
+
+    }
+
+    if(search.length<3){
+
+      setError('Movie name should be atleast 2 characters') 
+      return
+
+    }
+
+    setError(null)
+
+
+  },[search])
+
+  return {search, updateSearch, error}
+}
 
 function App() {
 
       // Destructure the `movies` array from the custom hook `useMovies`
       const {movies} = useMovies()
-
-
+      const {search,updateSearch, error} = useSearch()
+     
       // Handle form submission
       const handleSubmit = (event) => {
         // Prevent the default form submission behavior (e.g., page reload)
         event.preventDefault()
-        // Extract the `query` value from the submitted form data
-        const {query} = Object.fromEntries(new FormData(event.target))
-        console.log({query})
+        console.log({search})
       }
 
+      const handleChange = (event) => {
+        updateSearch(event.target.value)
+      }
+
+
+      
 
   return (
    
@@ -30,9 +79,10 @@ function App() {
           <h1>Movie Search</h1>
           <form className='form' onSubmit={handleSubmit}>
 
-            <input name="query" placeholder='Avengers, Star Wars, Mad Max...' />
+            <input onChange={handleChange} value={search} name="query" placeholder='Avengers, Star Wars, Mad Max...' />
             <button type='submit'>Search</button>
           </form>
+          {error && <p style={{color:'red'}}>{error}</p>}
         </header>
 
         <main>
