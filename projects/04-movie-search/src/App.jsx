c15a15cf79e6c133cc41c77e04156bc1a8527888
Manuel Ruiz Toribio <mrt00033@red.ujaen.es>
2024-08-19 12:01:31 +0200

    Debounce added to avoid making too many API calls

--- projects/04-movie-search/src/App.jsx
+++ projects/04-movie-search/src/App.jsx
@@ -2,12 +2,12 @@
 import './App.css'
 import { useMovies } from './hooks/useMovies.js'
 import { Movies } from './components/Movies.jsx'
-import { useEffect, useState, useRef, useMemo } from 'react'
-
+import { useEffect, useState, useRef, useMemo, useCallback } from 'react'
+import debounce from 'just-debounce-it'
 
 // Custom hook to handle search input
-function useSearch(){
-  const [search,updateSearch] = useState('')
+function useSearch() {
+  const [search, updateSearch] = useState('')
   const [error, setError] = useState(null)
   //useRef to check if it is the first input and not show error message
   const isFirstInput = useRef(true)
@@ -15,29 +15,29 @@ function useSearch(){
 
   useEffect(() => {
 
-    if(isFirstInput.current){
+    if (isFirstInput.current) {
       //set isFirstInput to false after first input
-      isFirstInput.current = search==''
+      isFirstInput.current = search == ''
       return
     }
 
-    if(search==''){
+    if (search == '') {
 
-      setError('Please enter a movie name') 
-      return
-
-    }
-
-    if(search.match(/^\d+$/)){
-
-      setError('Please enter a valid movie name') 
+      setError('Please enter a movie name')
       return
 
     }
 
-    if(search.length<3){
+    if (search.match(/^\d+$/)) {
 
-      setError('Movie name should be atleast 2 characters') 
+      setError('Please enter a valid movie name')
+      return
+
+    }
+
+    if (search.length < 3) {
+
+      setError('Movie name should be atleast 2 characters')
       return
 
     }
@@ -45,62 +45,79 @@ function useSearch(){
     setError(null)
 
 
-  },[search])
+  }, [search])
 
-  return {search, updateSearch, error}
+  return { search, updateSearch, error }
 }
 
 function App() {
 
-      const [sort, setSort] = useState(false)
-      const {search,updateSearch, error} = useSearch()
-      const {movies,loading,getMovies} = useMovies({search,sort})
-      // Handle form submission
-      const handleSubmit = (event) => {
-        // Prevent the default form submission behavior (e.g., page reload)
-        event.preventDefault()
-        getMovies()
-      }
-
-      const handleChange = (event) => {
-        updateSearch(event.target.value)
-      }
-
-      const handleSort = () => {
-        setSort(!sort)
+  const [sort, setSort] = useState(false)
+  const { search, updateSearch, error } = useSearch()
+  const { movies, loading, getMovies } = useMovies({ search, sort })
 
 
-      }
-      
+  // Debounce the getMovies function to avoid making too many API calls and improve performance
+  //useCallback to avoid creating a new function on every render
+  const debouncedGetMovies = useCallback(
+
+    debounce(search => {
+      console.log('search', search)
+      getMovies({ search })
+
+    }, 300)
+    , [getMovies]
+  )
+
+
+  // Handle form submission
+  const handleSubmit = (event) => {
+    // Prevent the default form submission behavior (e.g., page reload)
+    event.preventDefault()
+    getMovies(search)
+  }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    updateSearch(event.target.value)
+    debouncedGetMovies(newSearch)
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
+
+
+  }
+
 
   return (
-   
 
 
-      <div className="page">
-        <header>
-          <h1>Movie Search</h1>
-          <form className='form' onSubmit={handleSubmit}>
 
-            <input onChange={handleChange} value={search} name="query" placeholder='Avengers, Star Wars, Mad Max...' />
-            <input type='checkbox' onChange={handleSort} checked={sort} />
-            <button type='submit'>Search</button>
-          </form>
-          {error && <p style={{color:'red'}}>{error}</p>}
-        </header>
+    <div className="page">
+      <header>
+        <h1>Movie Search</h1>
+        <form className='form' onSubmit={handleSubmit}>
 
-        <main>
+          <input onChange={handleChange} value={search} name="query" placeholder='Avengers, Star Wars, Mad Max...' />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
+          <button type='submit'>Search</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </header>
 
-          {
-            loading ? <p>Loading...</p> : <Movies movies={movies} />
+      <main>
+
+        {
+          loading ? <p>Loading...</p> : <Movies movies={movies} />
 
 
-          }
+        }
 
-          
 
-        </main>
-      </div>
+
+      </main>
+    </div>
 
 
   )
--- projects/04-movie-search/src/hooks/useMovies.js
+++ projects/04-movie-search/src/hooks/useMovies.js
@@ -9,31 +9,38 @@ export function useMovies({ search, sort }) {
   // Create a ref to store the previous search term and avoid unnecessary API calls
   const previousSearch = useRef(search)
 
-  const getMovies = async () => {
-    if (search === previousSearch.current) {
-      return
+  // Create a memoized function to fetch movies from the API
+  //Instead of useMemo we could use useCallback, which is similar to useMemo but is used for functions instead of values
+  const getMovies = useMemo( () => {
+    return async ({search}) => {
+      if (search === previousSearch.current) {
+        return
+      }
+  
+      try {
+        setLoading(true)
+        setError(null)
+        previousSearch.current = search
+        const newMovies = await searchMovies({ search })
+        setMovies(newMovies)
+  
+      }catch(e){
+        setError(e.message)
+  
+      
+      }finally{
+        // Set loading to false regardless of success or failure
+        setLoading(false)
+      } 
+
     }
-
-    try {
-      setLoading(true)
-      setError(null)
-      previousSearch.current = search
-      const newMovies = await searchMovies({ search })
-      setMovies(newMovies)
-
-    }catch(e){
-      setError(e.message)
-
     
-    }finally{
-      // Set loading to false regardless of success or failure
-      setLoading(false)
-    } 
-  }
-  // Sort the movies by title if the sort flag is enabled
-  //const sortMovies = sort ? [...movies].sort((a, b) => a.title.localeCompare(b.title)) : movies
+  },[])
 
+
+  // Sort the movies by title if the sort flag is enabled
   // Use the useMemo hook to avoid sorting the movies on every render
+  //It will only sort the movies when the sort flag or the movies array changes
   const sortedMovies = useMemo(() => {
 
     return sort ? [...movies].sort((a, b) => a.title.localeCompare(b.title)) : movies
